c755ce30bc77fa98889971d8292f0ffeb0f7dace
Vincent Chee <vincent.kh.chee@gmail.com>
2021-05-04 17:22:16 +0800

    wip
    - constructor tests wip
    - execute txn's tests wip

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -4,6 +4,7 @@ import { ethers } from "hardhat";
 import { MultiSigWallet, MultiSigWalletFactory } from "../typechain";
 
 describe("MultiSigWallet Constructor Tests", function () {
+  let multiSigWallet: MultiSigWallet;
   let multiSigWalletFactory: MultiSigWalletFactory;
   let owner0: SignerWithAddress;
   let owner1: SignerWithAddress;
@@ -24,27 +25,46 @@ describe("MultiSigWallet Constructor Tests", function () {
   });
 
   it("Should not initialize if the number of required confirmations is 0", async () => {
-    await expect(multiSigWalletFactory.deploy([owner0.address], 0)).to.be.revertedWith(
-      "invalid number of required confirmations"
-    );
+    await expect(
+      multiSigWalletFactory.deploy([owner0.address], 0)
+    ).to.be.revertedWith("invalid number of required confirmations");
   });
 
   it("Should not initialize if the number of required confirmations is greater than the number of owners", async () => {
-    await expect(multiSigWalletFactory.deploy([owner0.address], 2)).to.be.revertedWith(
-      "invalid number of required confirmations"
-    );
+    await expect(
+      multiSigWalletFactory.deploy([owner0.address], 2)
+    ).to.be.revertedWith("invalid number of required confirmations");
   });
 
   it("Should not initialize if owner is address 0", async () => {
-    await expect(multiSigWalletFactory.deploy([ethers.constants.AddressZero], 1)).to.be.revertedWith(
-      "invalid owner"
-    );
+    await expect(
+      multiSigWalletFactory.deploy([ethers.constants.AddressZero], 1)
+    ).to.be.revertedWith("invalid owner");
   });
-  
+
   it("Should not initialize with duplicate owners", async () => {
-    await expect(multiSigWalletFactory.deploy([owner0.address, owner0.address], 1)).to.be.revertedWith(
-      "owner not unique"
+    await expect(
+      multiSigWalletFactory.deploy([owner0.address, owner0.address], 1)
+    ).to.be.revertedWith("owner not unique");
+  });
+
+  it("Should initialize if num owners === numConfirmationsRequired", async () => {
+    multiSigWallet = await multiSigWalletFactory.deploy(
+      [owner0.address, owner1.address],
+      2
     );
+    expect(await multiSigWallet.getOwners()).to.eql([
+      owner0.address,
+      owner1.address,
+    ]);
+  });
+
+  it("Should initialize if num owners > numConfirmationsRequired", async () => {
+    await multiSigWalletFactory.deploy([owner0.address, owner1.address], 1);
+    expect(await multiSigWallet.getOwners()).to.eql([
+      owner0.address,
+      owner1.address,
+    ]);
   });
 });
 
@@ -155,6 +175,89 @@ describe("MultiSigWallet Confirm Transaction Tests", function () {
     await multiSigWallet.submitTransaction(nonOwner0.address, 0, []);
     await expect(multiSigWallet.confirmTransaction(0))
       .to.emit(multiSigWallet, "ConfirmTransaction")
+      .withArgs(owner0.address, 0);
+  });
+});
+
+describe("MultiSigWallet Execute Transaction Tests", function () {
+  let multiSigWallet: MultiSigWallet;
+  let multiSigWalletFactory: MultiSigWalletFactory;
+  let owner0: SignerWithAddress;
+  let owner1: SignerWithAddress;
+  let owner2: SignerWithAddress;
+  let nonOwner0: SignerWithAddress;
+
+  before(async () => {
+    [owner0, owner1, owner2, nonOwner0] = await ethers.getSigners();
+    multiSigWalletFactory = (await ethers.getContractFactory(
+      "MultiSigWallet",
+      owner0
+    )) as MultiSigWalletFactory;
+  });
+
+  beforeEach(async () => {
+    multiSigWallet = (await multiSigWalletFactory.deploy(
+      [owner0.address, owner1.address, owner2.address],
+      2
+    )) as MultiSigWallet;
+    await multiSigWallet.deployed();
+  });
+
+  it("Execute Transaction should not work if sent by non-owner", async () => {
+    await multiSigWallet.submitTransaction(nonOwner0.address, 10, []);
+    await expect(
+      multiSigWallet.connect(nonOwner0).executeTransaction(0)
+    ).to.be.revertedWith("not owner");
+  });
+
+  it("Execute Transaction should not work if txn doesn't exist", async () => {
+    await expect(
+      multiSigWallet.connect(owner0).executeTransaction(0)
+    ).to.be.revertedWith("tx does not exist");
+  });
+
+  it("Execute Transaction should not work if txn already executed", async () => {
+    await multiSigWallet.submitTransaction(nonOwner0.address, 0, []);
+
+    await multiSigWallet.connect(owner0).confirmTransaction(0);
+    await multiSigWallet.connect(owner1).confirmTransaction(0);
+
+    await multiSigWallet.executeTransaction(0);
+
+    await expect(multiSigWallet.executeTransaction(0)).to.be.revertedWith(
+      "tx already executed"
+    );
+  });
+
+  it("Execute Transaction should not work if number of confirmations is less than numConfirmationsRequired", async () => {
+    await multiSigWallet.submitTransaction(nonOwner0.address, 10, []);
+
+    await multiSigWallet.connect(owner0).confirmTransaction(0);
+
+    await expect(multiSigWallet.executeTransaction(0)).to.be.revertedWith(
+      "cannot execute tx"
+    );
+  });
+
+  it("Execute Transaction fails when txn value greater than 0, but data is empty", async () => {
+    await multiSigWallet.submitTransaction(nonOwner0.address, 10, []);
+
+    await multiSigWallet.connect(owner0).confirmTransaction(0);
+    await multiSigWallet.connect(owner1).confirmTransaction(0);
+
+    await expect(multiSigWallet.executeTransaction(0)).to.be.revertedWith(
+      "tx failed"
+    );
+  });
+
+  it("Execute Transaction should emit the correct event and args", async () => {
+    await multiSigWallet.submitTransaction(nonOwner0.address, 0, []);
+
+    await multiSigWallet.connect(owner0).confirmTransaction(0);
+    await multiSigWallet.connect(owner1).confirmTransaction(0);
+
+    await expect(multiSigWallet.executeTransaction(0))
+      .to.emit(multiSigWallet, "ExecuteTransaction")
       .withArgs(owner0.address, 0);
   });
 });
