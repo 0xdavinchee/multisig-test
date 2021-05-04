321bde351b63b84ccdba673426daa83295d996e5
Vincent Chee <vincent.kh.chee@gmail.com>
2021-05-04 12:49:05 +0800

    wip
    - multisig submit txn tests wip

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Feel free to choose a different contract of your own creation or one that you li
 1. Using the ethers.js library create a ContractFactory object and use it to deploy an instance of the Multi-Sig contract to a local hardhat network.
 
 2. Write tests for the Multi-Sig contract to reach a 40% coverage.
-Note: a few things that are important to test are. The submitTransaction,  confirmTransaction, executeTransaction, and revokeConfirmation worked as expected and emit the correct event and arguments. That the modifiers onlyOwner, txExists, notExecuted, and notConfirmed work as expected. That the getTransaction method returns the right data.
+Note: a few things that are important to test are. The submitTransaction, confirmTransaction, executeTransaction, and revokeConfirmation worked as expected and emit the correct event and arguments. That the modifiers onlyOwner, txExists, notExecuted, and notConfirmed work as expected. That the getTransaction method returns the right data.
 
 3. Create a gas report of the cost of deploying a new Multi-Sig contract and how much gas cost calling the different methods.
 
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,14 +1,39 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { MultiSigWallet } from "../typechain/MultiSigWallet";
+import { MultiSigWallet, MultiSigWalletFactory } from "../typechain";
 
-describe("MultiSigWallet", function () {
+describe("MultiSigWallet Submit Transaction Tests", function () {
   let multiSigWallet: MultiSigWallet;
-  it("Should return the new greeting once it's changed", async function () {
-    const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
-    // multiSigWallet = (await MultiSigWallet.deploy(
-    // )) as MultiSigWallet;
+  let multiSigWalletFactory: MultiSigWalletFactory;
+  let owner0: SignerWithAddress;
+  let owner1: SignerWithAddress;
+  let nonOwner0: SignerWithAddress;
 
-    // await multiSigWallet.deployed();
+  before(async () => {
+    [owner0, owner1, nonOwner0] = await ethers.getSigners();
+    multiSigWalletFactory = (await ethers.getContractFactory(
+      "MultiSigWallet",
+      owner0
+    )) as MultiSigWalletFactory;
+  });
+
+  beforeEach(async () => {
+    multiSigWallet = (await multiSigWalletFactory.deploy(
+      [owner0.address],
+      1
+    )) as MultiSigWallet;
+    await multiSigWallet.deployed();
+  });
+  
+  it("Submit Transaction should work if sent by owner", async () => {
+    await multiSigWallet.submitTransaction(owner0.address, 0, []);
+    expect((await multiSigWallet.getTransactionCount()).toNumber()).to.eq(1);
+  });
+
+  it("Submit Transaction should emit the correct event and args", async () => {
+    await expect(multiSigWallet.submitTransaction(owner1.address, 0, []))
+      .to.emit(multiSigWallet, "SubmitTransaction")
+      .withArgs(owner0.address, 0, owner1.address, 0, []);
   });
 });
